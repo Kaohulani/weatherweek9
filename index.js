c1cdb9ab3df4a12c3987996adb2ef5f950ff5539
Kaohulani <133470377+Kaohulani@users.noreply.github.com>
2024-03-10 23:34:49 +0000

    Week9hw

--- index.js
+++ index.js
@@ -4,49 +4,60 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//displays index.html of root path
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+// displays index.html of root path
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
 });
 
-//invoked after hitting go in the html form
-app.post("/", function(req, res) {
-    
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
-    
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
-        const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
-    
-    // this gets the data from Open WeatherPI
-    https.get(url, function(response){
+// invoked after hitting go in the html form
+app.post("/", function (req, res) {
+
+    // takes in the city name from the html form
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+
+    // build up the URL for the JSON query
+    const units = "imperial";
+    const apiKey = "873010304317811c87ca1b69b7854af9";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + latitude + longitude + "&units=" + units + "&APPID=" + apiKey;
+
+    // this gets the data from Open WeatherAPI
+    https.get(url, function (response) {
         console.log(response.statusCode);
-        
+
         // gets individual items from Open Weather API
-        response.on("data", function(data){
+        let data = '';
+        response.on("data", function (chunk) {
+            data += chunk;
+        });
+
+        response.on("end", function () {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
+          const cloudiness = weatherData.clouds.all;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            
+
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("<img src=" + imageURL +">");
+            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit</h2>");
+            res.write("<p>Humidity: " + humidity + "%</p>");
+            res.write("<p>Wind Speed: " + windSpeed + " mph</p>");
+          res.write("<p>Cloudiness: " + cloudiness + "%</p>")
+            res.write("<img src=" + imageURL + ">");
             res.send();
         });
     });
-})
+});
 
 
-//Code will run on 3000 or any available open port
-app.listen(process.env.PORT || 3000, function() {
-console.log ("Server is running on port")
+// Code will run on 3000 or any available open port
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port");
 });
